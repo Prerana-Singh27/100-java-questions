cc6762f388f4d2d1deadb400781028f56c6755ea
Prerana Kumari Singh <singhprerana199@gmail.com>
2025-08-02 13:20:07 +0530

    Update Array2D.java

--- src/Array2D.java
+++ src/Array2D.java
@@ -1,2 +1,31 @@
+import java.util.Scanner;
 public class Array2D {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to find 2DArray Searching\n");
+        int[][] numArr = ArrayUtility.input2DArray();
+        System.out.print("enter the number you want to search :");
+        int num = input.nextInt();
+        boolean isFound = isFound(numArr,num);
+        if(isFound){
+            System.out.println("your number was found in the array");
+        }else{
+            System.out.println("your number was not found in the array");
+        }
+    }
+    public static boolean isFound(int[][] numArr,int num){
+        int i = 0;
+        while(i < numArr.length){
+            int j =0;
+            while (j < numArr.length) {
+                if(numArr[j][i] == num){
+                    return true;
+                }
+                j++;
+            }
+            i++;
+        }
+        return false;
+
+    }
 }
