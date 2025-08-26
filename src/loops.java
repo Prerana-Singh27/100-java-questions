7e2ae462b7f0e21bbf971f4da46968175f3fc4f9
Prerana Kumari Singh <singhprerana199@gmail.com>
2025-08-26 11:06:24 +0530

    Update loops.java

--- src/loops.java
+++ src/loops.java
@@ -1,13 +1,17 @@
-//password checker
+//password checker(do-while)
 
 import java .util.scanner*;
 public class loops{
 public static void main (String[] args){
 Scanner input = new scanner(System.in);
-
+        String corrPassword = "shruti123";
+        String i;
+        do{
+            System.out.println("Enter password : ");
+            i = input.nextLine();
+        } while (!i.equals( corrPassword ));
+        System.out.println("Access granted!!!!");
 }
-
-
 }
 
 
