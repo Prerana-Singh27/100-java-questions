3e53cff6403dcb110329eae0da93e129010d5b01
Prerana Kumari Singh <singhprerana199@gmail.com>
2025-08-27 22:19:30 +0530

    Update loops.java

--- src/loops.java
+++ src/loops.java
@@ -82,3 +82,13 @@ public class loops {
     public static void main(String[] args) {
         System.out.println("WELCOME TO FINDING THE MAX ");
         int[] arr = ArrayUtility.inputArray();
+
+             int max = Integer.MIN_VALUE;
+        for (int num : arr) {
+            if (max < num) {
+                max = num;
+            }
+        }
+        System.out.println("Maximum number is " + max);
+    }
+}
