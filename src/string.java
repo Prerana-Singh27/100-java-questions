e8a1720cc6bd2efdd82da609a1a7f0c31aba23b4
Prerana Kumari Singh <singhprerana199@gmail.com>
2025-08-29 21:15:22 +0530

    Update string.java

--- src/string.java
+++ src/string.java
@@ -1,1 +1,20 @@
+// define student class with fields like name and age and use toString to print student details 
 
+public class string{
+  String name;
+  int age;
+//constructor
+public string(String name , int age ){
+  this.name = name;
+  this.age = age;
+}
+
+public String toString() {
+  return "Student Details : {name : " + name + ", age : " + age + "}";
+}
+  public static void main (String[] args){
+    string student = new string("shruti" , 20);
+    system.out.println(student);
+  }
+}
+    
