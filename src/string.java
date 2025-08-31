0b98cdd426a871177378f0c7d395d3786bb4baf8
Prerana Kumari Singh <singhprerana199@gmail.com>
2025-08-31 09:58:46 +0530

    Update string.java

--- src/string.java
+++ src/string.java
@@ -76,3 +76,18 @@ class string {
         }
     }
 }
+
+//array of words & concatenate them into a single string
+
+class string {
+    public static void main(String[] args) {
+        String[] arr = new String[] {
+                "Shruti" , "is" , "very","beautiful"
+        };
+        StringBuilder sb = new StringBuilder();
+        for(String str : arr) {
+            sb.append(str).append(" ");
+        }
+        System.out.println(sb);
+    }
+}
