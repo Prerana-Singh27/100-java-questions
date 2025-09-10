d5c147387ab4e87219957066fc446bbfa7192ea5
Prerana Kumari Singh <singhprerana199@gmail.com>
2025-09-10 17:49:47 +0530

    Update concatenateString.java

--- concatenateString.java
+++ concatenateString.java
@@ -1,1 +1,16 @@
 
+package Collection;
+class challenge1 {
+    public static String concatenate(String...elements) {
+        StringBuilder sb = new StringBuilder();
+        for (String element : elements) {
+            sb.append(element).append(" ");
+        }
+        return sb.toString();
+    }
+    public static void main(String[] args) {
+        System.out.println(concatenate(" shruti "," is "," a ", " beautiful "," girl !!!!"));
+        System.out.println( concatenate("Finally","my","java","course","is","over"));
+
+    }
+}
