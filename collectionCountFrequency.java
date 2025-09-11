ff92df8f87861b1e35d50df0974a8a355474240c
Prerana Kumari Singh <singhprerana199@gmail.com>
2025-09-11 18:45:45 +0530

    Update collectionCountFrequency.java

--- collectionCountFrequency.java
+++ collectionCountFrequency.java
@@ -1,1 +1,16 @@
+//Collection class to count freq of elementin ArrarList
+
+package Collection;
+
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Collections;
+
+class challenge3 {
+     public static void main(String[] args) {
+         List<Integer> list = Arrays.asList(1,4,6,6,8,9,6);
+         System.out.println(Collections.frequency(list,6));
+     }
+}
 
