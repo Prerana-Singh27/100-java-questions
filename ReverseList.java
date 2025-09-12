7832e76db253e5d616e5009c8638902cb7286502
Prerana Kumari Singh <singhprerana199@gmail.com>
2025-09-12 23:36:56 +0530

    Update ReverseList.java

--- ReverseList.java
+++ ReverseList.java
@@ -1,1 +1,25 @@
+//reverse the element of a list and print the reverse list
+
+package Collection;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class challenge5 {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(2, 3, 5, 6, 7, 8);
+        System.out.println(list);
+        reverse( list);
+        System.out.println(list);
+    }
+
+    public static void reverse(List<Integer> list) {
+        //Collections.reverse(list);
+
+        for (int i =0; i<list.size()/2; i++) {
+            ListSwapping.swap(list,i,list.size()-1-i);
+        }
+    }
+}
 
