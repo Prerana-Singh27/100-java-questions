d4d80045d7fbad53fd7d07d0d10c575388ddcc9d
Prerana Kumari Singh <singhprerana199@gmail.com>
2025-09-12 23:37:55 +0530

    Update uniqueCharacter.java

--- uniqueCharacter.java
+++ uniqueCharacter.java
@@ -1,1 +1,20 @@
 
+package Collection;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class UniqueCharacters {
+    public static void main(String[] args) {
+        Set<Character> unique = new HashSet<>();
+        Scanner input = new Scanner(System.in);
+        System.out.println("PLEASE WRITE YOUR STRING: ");
+        String userStr = input.next();
+
+        for (char ch : userStr.toCharArray()) {
+            unique.add(ch);
+        }
+        System.out.printf("your string has %d unique characters ", unique.size());
+    }
+}
