9f37e08d805ef6c69c20cdf608bc463586919636
Prerana Kumari Singh <singhprerana199@gmail.com>
2025-09-13 14:21:45 +0530

    Update enum3.java

--- enum3.java
+++ enum3.java
@@ -1,1 +1,12 @@
 // ENHANCE THE DAY ENUM BY WEEKDAY OR WEEKEND 
+
+package seeEnum;
+
+public class EnumTest {
+    public static void main(String[] args) {
+        System.out.println("Printing the days of the week:");
+        for (Day day : Day.values()) {
+          System.out.printf("%s : %s\n" , day,day.getType());        }
+   }
+}
+
