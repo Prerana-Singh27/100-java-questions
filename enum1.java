6f7501ab56889479d9e285b9aa25b85978ab7849
Prerana Kumari Singh <singhprerana199@gmail.com>
2025-09-13 14:12:37 +0530

    Update enum1.java

--- enum1.java
+++ enum1.java
@@ -1,1 +1,29 @@
+//crea
+package seeEnum;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+ class CountryMap {
+    public static void main(String[] args) {
+        Map<String, String> countryMap = new HashMap<>();
+
+        countryMap.put("India", "NewDelhi");
+        countryMap.put("china", "Beijing");
+        countryMap.put("Pakistan", "Islamabad");
+        countryMap.put("SriLanka", "Columbo");
+        countryMap.put("Bangladesh", "Dhaka");
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("PLEASE ENTER THE COUNTRY NAME: ");
+        String country = input.next();
+        if (countryMap.containsKey(country)) {
+            System.out.printf("country of %s is %s" , country,countryMap.get(country));
+        }else {
+            System.out.println("sorry");
+        }
+
+    }
+}
 
