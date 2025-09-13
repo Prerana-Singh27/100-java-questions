3076d2a83a6cada8ebf962b2e23e0c493a24b25a
Prerana Kumari Singh <singhprerana199@gmail.com>
2025-09-13 14:19:09 +0530

    Update enum2.java

--- enum2.java
+++ enum2.java
@@ -1,1 +1,26 @@
 // create enum represent days of week,prints out all days of the week
+
+package seeEnum;
+
+public enum Day {
+    Sunday(false),
+    Monday(true),
+    Tuesday(true),
+    Wednesday(true),
+    Thursday(true),
+    Friday(true),
+    Saturday(false),;
+
+    private final boolean isWeekday;
+
+    Day(boolean isWeekday) {
+        this.isWeekday = isWeekday;
+    }
+
+    public String getType() {
+        return isWeekday ? "weekday" : "weekend";
+    }
+}
+
+
+
