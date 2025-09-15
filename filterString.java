6269d29e71dedb70e4bd4a25987d6de9c8a7b11c
Prerana Kumari Singh <singhprerana199@gmail.com>
2025-09-15 20:29:42 +0530

    Update filterString.java

--- filterString.java
+++ filterString.java
@@ -1,1 +1,16 @@
+//list of strings,use stream operations to filter out Strings that have length of 10 or more and then concate the remaining Strings
+
+package functionalProgramming;
+
+import java.util.List;
+
+public class filterString {
+    public static void main(String[] args) {
+        List<String> strs = List.of("SHOBHASINGH" , "PRERANA" , "SHRUTISINGH");
+
+      String result =   strs.stream().filter(name -> name.length() > 10)
+                .reduce( "", (a , b) -> a + " " + b);
+        System.out.println(result);
+    }
+}
 
