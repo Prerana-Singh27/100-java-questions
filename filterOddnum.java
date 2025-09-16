c5af06c5c96c8f016563ba2ec14d9f51cd28d9aa
Prerana Kumari Singh <singhprerana199@gmail.com>
2025-09-16 10:27:51 +0530

    Update filterOddnum.java

--- filterOddnum.java
+++ filterOddnum.java
@@ -1,1 +1,14 @@
+package functionalProgramming;
+
+
+import java.util.List;
+
+public class filterOddnum {
+    public static void main(String[] args) {
+        List<Integer> nums = List.of(2,4,7,9,3,87,37,57);
+        nums.stream()
+                .filter( number -> number % 2 != 0)
+                .forEach(number -> System.out.println(number));
+    }
+}
 
