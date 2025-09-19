798cf2f1b12ecf34a9007082755808ea9e027348
Prerana Kumari Singh <singhprerana199@gmail.com>
2025-09-19 11:09:07 +0530

    Update trafficSignalThread.java

--- trafficSignalThread.java
+++ trafficSignalThread.java
@@ -1,1 +1,18 @@
+package thread;
+
+public enum TraffiColor {
+    RED (9000),
+    YELLOW(10000) ,
+    GREEN(3000);
+
+    private final int time;
+
+    TraffiColor(int time) {
+        this.time = time;
+    }
+
+    public int getTime() {
+        return time;
+    }
+}
 
