137367c7fc5dae60ef46d45c17cb7707ccb6de8c
Prerana Kumari Singh <singhprerana199@gmail.com>
2025-09-19 11:42:17 +0530

    Update executorService-singleThread.java

--- executorService-singleThread.java
+++ executorService-singleThread.java
@@ -1,1 +1,24 @@
+package ExecutorService;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class singleThread implements Runnable {
+
+    @Override
+    public void run() {
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("\nTHE NUMBER FROM 1-10 IS : %d ", i);
+        }
+    }
+
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Runnable task = new singleThread();
+
+        executor.submit(task);
+        executor.shutdown();
+    }
+}
+
 
