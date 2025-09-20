23ba049d75a8637a12ba24d6e21fb0efa46927bf
Prerana Kumari Singh <singhprerana199@gmail.com>
2025-09-20 15:17:59 +0530

    Update twoversions.java

--- twoversions.java
+++ twoversions.java
@@ -1,1 +1,22 @@
+package functionalInterface;
+
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class twoVersions {
+    public static void main(String[] args ) {
+        System.out.print("enter your number:");
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+        int fact = 1;
+        for (int i = 1; i <= num; i++) {
+            fact *= i;
+        }
+        System.out.println("factorial of " + num + "is:" + fact);
+
+        IntStream.rangeClosed(2, num) .reduce((a, b) -> a * b)
+        .ifPresent(System.out::println);
+    }
+    }
+
 
