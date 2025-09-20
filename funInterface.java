32bafd903e7dccd30c5d96427f9174c24a8c6251
Prerana Kumari Singh <singhprerana199@gmail.com>
2025-09-20 15:15:46 +0530

    Update funInterface.java

--- funInterface.java
+++ funInterface.java
@@ -1,1 +1,22 @@
+//single abstract method--that accepts an integer and return a boolean--implement using lambda --cracks if the no is prime.
+
+
+package functionalInterface;
+
+public class funInterface {
+    public static void main(String[] args) {
+        Candidate candidate = num -> {
+            for (int i = 2; i < num; i++) {
+                if (num % i == 0) {
+                    return false;
+                }
+            }
+            return true;
+        };
+        System.out.println(candidate.isCandidate(2));
+    }
+    public interface Candidate {
+        boolean isCandidate(int num);
+    }
+}
 
