ae0db00bf7fb29777a0fc9c2337cdf4d443b7ebf
Prerana Kumari Singh <singhprerana199@gmail.com>
2025-11-04 22:55:03 +0530

    Fix formatting of day printing in EnumTest

--- enum3.java
+++ enum3.java
@@ -6,7 +6,8 @@ public class EnumTest {
     public static void main(String[] args) {
         System.out.println("Printing the days of the week:");
         for (Day day : Day.values()) {
-          System.out.printf("%s : %s\n" , day,day.getType());        }
+          System.out.printf("%s : %s\n" , day,day.getType());
+        }
    }
 }
 
